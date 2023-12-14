e7c497ccdc35cc28800fa11e579b2d461417cc78
FilosofDanil <serhio3347@gmail.com>
2023-12-14 12:11:00 +0100

    Added enum

---
+++ src/main/java/com/example/opituvalnik/enums/State.java
@@ -0,0 +1,12 @@
+package com.example.opituvalnik.enums;
+
+public enum State {
+    EMPTY,
+    SEARCHING,
+    CREATE_SURVEY,
+    SURVEY_PHOTO,
+    RIGHT_WRONG,
+    ADD_QUESTIONS,
+    QUESTION_CREATION,
+    MENU
+}

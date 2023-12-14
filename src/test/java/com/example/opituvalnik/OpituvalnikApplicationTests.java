c61d88ddbed8083bd7cecd9fcb95d58bf20a1b2e
FilosofDanil <serhio3347@gmail.com>
2023-12-14 12:06:57 +0100

    created an empty project

---
+++ src/test/java/com/example/opituvalnik/OpituvalnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opituvalnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpituvalnikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

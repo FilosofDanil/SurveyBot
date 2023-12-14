5ce62c2d4865661e644c21e9ad7ac3436c645dbc
FilosofDanil <serhio3347@gmail.com>
2023-12-14 12:10:15 +0100

    Configs

---
+++ src/main/java/com/example/opituvalnik/configs/BotConfigs.java
@@ -0,0 +1,88 @@
+package com.example.opituvalnik.configs;
+
+import com.example.telelibrary.configs.BotConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource({"application.yml"})
+public class BotConfigs extends BotConfig {
+    @Value("${bot.name}")
+    private String name;
+    @Value("${bot.token}")
+    private String token;
+
+    public BotConfigs() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setToken(final String token) {
+        this.token = token;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof BotConfigs)) {
+            return false;
+        } else {
+            BotConfigs other = (BotConfigs)o;
+            if (!other.canEqual(this)) {
+                return false;
+            } else {
+                Object this$name = this.getName();
+                Object other$name = other.getName();
+                if (this$name == null) {
+                    if (other$name != null) {
+                        return false;
+                    }
+                } else if (!this$name.equals(other$name)) {
+                    return false;
+                }
+
+                Object this$token = this.getToken();
+                Object other$token = other.getToken();
+                if (this$token == null) {
+                    if (other$token != null) {
+                        return false;
+                    }
+                } else if (!this$token.equals(other$token)) {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof BotConfigs;
+    }
+
+    public int hashCode() {
+        boolean PRIME = true;
+        int result = 1;
+        Object $name = this.getName();
+        result = result * 59 + ($name == null ? 43 : $name.hashCode());
+        Object $token = this.getToken();
+        result = result * 59 + ($token == null ? 43 : $token.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        String var10000 = this.getName();
+        return "BotConfig(name=" + var10000 + ", token=" + this.getToken() + ")";
+    }
+}
---
+++ src/main/java/com/example/opituvalnik/configs/MyConfiguration.java
@@ -0,0 +1,7 @@
+package com.example.opituvalnik.configs;
+
+import com.example.telelibrary.configs.Configuration;
+
+@org.springframework.context.annotation.Configuration
+public class MyConfiguration extends Configuration {
+}
